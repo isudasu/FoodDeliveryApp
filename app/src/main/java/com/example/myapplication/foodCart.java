c8a95fc6449e86259f8a8ee0fe60a2e513dc2ad4
isudasu <dasu.np2000@gmail.com>
2019-09-09 21:30:30 +0530

    das

--- app/src/main/java/com/example/myapplication/foodCart.java
+++ app/src/main/java/com/example/myapplication/foodCart.java
@@ -2,11 +2,15 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class foodCart extends AppCompatActivity {
 
+    Button placeOrder;
     TextView cartList,priceView;
     String list_food;
     double price1;
@@ -18,6 +22,13 @@ public class foodCart extends AppCompatActivity {
 
         cartList = findViewById(R.id.cartList);
         priceView = findViewById(R.id.priceView);
+        placeOrder = (Button) findViewById(R.id.placeOrder);
+
+        placeOrder.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                openactivity3();
+            }
+        });
 
         Bundle bundle = getIntent().getExtras();
         list_food = bundle.getString("food");
@@ -26,5 +37,10 @@ public class foodCart extends AppCompatActivity {
 
          cartList.setText(list_food);
         priceView.setText(String.valueOf(price1));
+    }
+
+    public void openactivity3(){
+        Intent intent = new Intent(foodCart.this,Main3Activity.class);
+        startActivity(intent);
     }
 }
